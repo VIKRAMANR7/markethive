92d909eb76030c8b774729f2a9a6147a715bfda4
Vikraman <vkram777@gmail.com>
2025-10-11 16:52:10 +0530

    Layout setup, shadcnui,themeprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { ThemeToggle } from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-2xl text-blue-500">Welcome</h1>
+    <div className="p-5">
+      <div className="w-full flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="font-bold text-2xl text-blue-500 font-barlow">Welcome</h1>
+      <Button>Sign Up</Button>
     </div>
   );
 }
